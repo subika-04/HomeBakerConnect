0181a24bf9900d48ca0c6f116bce317de09e78f8
subika-04 <subikapro@gmail.com>
2026-02-09 19:31:26 +0530

    updated1

--- frontend-react/src/pages/product/UserOrder.jsx
+++ frontend-react/src/pages/product/UserOrder.jsx
@@ -3,6 +3,13 @@ import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../css/UserOrder.css";
+import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 const UserOrder = () => {
   const [orders, setOrder] = useState([]);
